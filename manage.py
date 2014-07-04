cf9048d2ab2258869d7f83af7788496d90b4138e
yuzhenquan <2543352661@qq.com>
2014-07-04 16:17:51 +0800

    add git test

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
+#hello everyone ,this is a test for git.
+#this is a second test.
 #!/usr/bin/env python
 import os
-import sys
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeworkManager.settings")
