a7a147bd0e3d6ae0ad588d1958d6be80b68f8dac
yuzhenquan <2543352661@qq.com>
2014-07-07 14:47:31 +0800

    add learngit

---
+++ learngit/manage.py
@@ -0,0 +1,13 @@
+#hello everyone ,this is a test for git.
+#this is a second test.
+#!/usr/bin/env python
+import os
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeworkManager.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+add this words to illustrate the git effect.
